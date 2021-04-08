f49fe250cdade11a31e0424a531e08974b5e3401
adepentane <adepentane5@gmail.com>
2021-04-08 01:11:31 +0100

    Added conditionals, loops, whileLoops files

---
+++ conditionals.py
@@ -0,0 +1,17 @@
+age = 17
+height = 5
+if(age >= 18 and height):
+    print('This Person can get on the ride')
+if(age < 18 or height < 5):
+    print('Person cannot get on this ride')
+
+
+
+
+# movieName = "Captain America"
+
+#if (age >= 18):
+#    print('Customer can Purchase Ticket now for ' + movieName + ' movie')
+#if(age < 18):
+#    print('Customer Blocked from watching ' + movieName)
+
---
+++ loops.py
@@ -0,0 +1,4 @@
+alist = [1,2,3,4,5,6,7,8,9]
+for x in range(1001):
+    if(not(x == 0)):
+        print(x)
